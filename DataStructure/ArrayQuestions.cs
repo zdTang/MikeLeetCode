deb3e4bba2262d46aa3f76b01461a68ee43977e2
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-13 20:02:43 -0400

    [Array] Count how many words in a sentence

---
+++ DataStructure/ArrayQuestions.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStructure
+{
+    public static class ArrayQuestions
+    {
+        //public static string Sentence = "I have 50 books in my 2 bookshelves";
+        public static void Test()
+        {
+            var result = CountWords("I have 50 books in my 2 bookshelves");
+            Console.WriteLine(result);
+        }
+
+
+        /// <summary>
+        /// count how many non-number words in a given sentence
+        /// </summary>
+        /// <param name="sentence"></param>
+        /// <returns></returns>
+
+        public static int CountWords(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            int count = 0;
+            foreach (string word in words)
+            {
+                if (!int.TryParse(word, out int thisWord))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
--- MikeLeetCode/Program.cs
+++ MikeLeetCode/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("===The test result is as follow:=====");
+
+            // Data structure
+            DataStructure.ArrayQuestions.Test();
         }
     }
 }
