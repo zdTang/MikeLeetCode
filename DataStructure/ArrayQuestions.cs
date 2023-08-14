ae21926e34957a8f32a3aba86da68b1d98db3c30
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-13 22:56:03 -0400

    [Array-String]  Palindrome Number
    Leetcode  Palindrome Number

--- DataStructure/ArrayQuestions.cs
+++ DataStructure/ArrayQuestions.cs
@@ -1,12 +1,18 @@
-﻿namespace DataStructure
+﻿using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DataStructure
 {
     public static class ArrayQuestions
     {
         //public static string Sentence = "I have 50 books in my 2 bookshelves";
         public static void Test()
         {
-            var result = CountWords("I have 50 books in my 2 bookshelves");
+            //var result = CountWords("I have 50 books in my 2 bookshelves");
+            var result=IfPalindrome(121);
             Console.WriteLine(result);
+            var result2 = IfPalindrome(-121);
+            Console.WriteLine(result2);
         }
 
 
@@ -30,5 +36,30 @@
 
             return count;
         }
+
+
+        //[Leetcode]9. Palindrome Number
+        //Given an integer x, return true if x is a palindrome, and false otherwise.
+        /*
+         * Example 1:
+
+            Input: x = 121
+            Output: true
+            Explanation: 121 reads as 121 from left to right and from right to left.
+            Example 2:
+
+            Input: x = -121
+            Output: false
+            Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+         */
+
+        public static bool IfPalindrome(int number) {
+            var forwardString = number.ToString();
+            var backwardString = forwardString.ToCharArray();  // string to char[]
+            Array.Reverse(backwardString);                     // Array reverse -- static
+            var backwardString2 = new string(backwardString);  // Instantiate an string with char[]
+            return forwardString.Equals(backwardString2);      // Comparison
+        }
+
     }
 }
